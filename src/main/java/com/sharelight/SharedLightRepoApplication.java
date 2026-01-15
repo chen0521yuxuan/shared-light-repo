965e1342baa82c4ae8768c64f542110fb3bfa5cb
Aaron Chen <chen0521yuxuan@qq.com>
2026-01-15 18:58:13 +0800

    initial

---
+++ src/main/java/com/sharelight/SharedLightRepoApplication.java
@@ -0,0 +1,13 @@
+package com.sharelight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedLightRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedLightRepoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sharelight/SharedLightRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sharelight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedLightRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
